024f8750be4faee579448014d0686f484abd0875
Salvador B <salvador.briones@selectra.info>
2019-03-16 18:19:01 +0100

    Adding IDE helper v2

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.7.20 on 2019-03-16 17:14:36.
+ * Generated for Laravel 5.7.20 on 2019-03-16 17:17:43.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
--- app/Market.php
+++ app/Market.php
@@ -5,6 +5,26 @@ namespace App;
 use App\Traits\ValidatorTrait;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Market
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $description
+ * @property int $active
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Market newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Market newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Market query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Market whereActive($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Market whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Market whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Market whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Market whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Market whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Market extends Model
 {
     use ValidatorTrait;
