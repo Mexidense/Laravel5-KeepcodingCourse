f5288b5b19343a1368120818e55cf0e3b1cd37d3
Salvador Briones <salvador.briones@selectra.info>
2019-03-25 18:50:27 +0100

    kind of callings to API created: average 6, 70 and 200.

--- app/Http/Controllers/StockHistoricalController.php
+++ app/Http/Controllers/StockHistoricalController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Helpers\HelperAlphavantage;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Config;
 
 class StockHistoricalController extends Controller
 {
@@ -19,8 +19,37 @@ class StockHistoricalController extends Controller
         dd(HelperAlphavantage::processArray($stockValues));
     }
 
-    public function create(Request $request)
-    {
+    public function saveStockHistorical($stock) {
+        $stockValuesProcessed = $this->getStockClosingValues($stock);
+        $smaProcessed6        = $this->getStockSMA($stock, 6);
+        $smaProcessed70       = $this->getStockSMA($stock, 70);
+        $smaProcessed200      = $this->getStockSMA($stock, 200);
+    }
 
+    private function getStockClosingValues($stock)
+    {
+        $params = [
+            'function'   => Config::get('larastock.closing_values_method'),
+            'symbol'     => $stock,
+            'outputsize' => 'compact',
+        ];
+
+        $stockValues = HelperAlphavantage::getArrayReply($params);
+        return HelperAlphavantage::processArray($stockValues);
+    }
+
+    private function getStockSMA($stock, $smaPeriod)
+    {
+        $params = [
+            'function'   => Config::get('larastock.moving_average_values_method'),
+            'symbol'     => $stock,
+            'interval'   => 'daily',
+            'time_period'=> $smaPeriod,
+            'series_type'=> 'close',
+            'outputsize' => 'compact',
+        ];
+
+        $sma = HelperAlphavantage::getArrayReply($params);
+        return HelperAlphavantage::processArray($sma);
     }
 }
--- config/larastock.php
+++ config/larastock.php
@@ -1,6 +1,8 @@
 <?php
 
 return [
-    'alphavantage_key'      => env('ALPHAVANTAGE_KEY', 'demo'),
-    'alphavantage_base_url' => 'https://www.alphavantage.co/query',
+    'alphavantage_key'             => env('ALPHAVANTAGE_KEY', 'demo'),
+    'alphavantage_base_url'        => 'https://www.alphavantage.co/query',
+    'closing_values_method'        => 'TIME_SERIES_DAILY',
+    'moving_average_values_method' => 'SMA',
 ];
